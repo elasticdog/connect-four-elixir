f5198d0b91aa7f523e736af8f7990bf7b85c6852
Aaron Bull Schaefer <aaron@elasticdog.com>
2015-11-04 21:04:15 -0800

    Add Board and Space modules

---
+++ lib/connect_four/board.ex
@@ -0,0 +1,20 @@
+defmodule ConnectFour.Board do
+  use Supervisor
+
+  @registered_name ConnectFourBoard
+  @last_row 6
+  @last_column 7
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, :ok, [name: @registered_name])
+  end
+
+  def init(:ok) do
+    children = for t <- spaces, do: worker(ConnectFour.Space, [t], id: t)
+    supervise(children, strategy: :one_for_one)
+  end
+
+  def spaces do
+    for row <- 1..@last_row, column <- 1..@last_column, do: {row, column}
+  end
+end

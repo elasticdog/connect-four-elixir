0fabd5c5cacb0114e531819ad3612899b8ddb6ef
Aaron Bull Schaefer <aaron@elasticdog.com>
2015-11-05 14:22:14 -0800

    Implement printing the board state

--- lib/connect_four/board.ex
+++ lib/connect_four/board.ex
@@ -3,18 +3,46 @@ defmodule ConnectFour.Board do
 
   @registered_name ConnectFourBoard
   @last_row 6
-  @last_column 7
+  @last_col 7
 
   def start_link do
     Supervisor.start_link(__MODULE__, :ok, [name: @registered_name])
   end
 
   def init(:ok) do
-    children = for t <- spaces, do: worker(ConnectFour.Space, [t], id: t)
-    supervise(children, strategy: :one_for_one)
+    for space <- spaces do
+      worker(ConnectFour.Space, [space], id: space)
+    end
+    |> supervise(strategy: :one_for_one)
   end
 
   def spaces do
-    for row <- 1..@last_row, column <- 1..@last_column, do: {row, column}
+    for row <- 1..@last_row, col <- 1..@last_col, do: {row, col}
+  end
+
+  def print do
+    for row <- @last_row..1, do: print_columns(row)
+  end
+
+  def print_columns(row) do
+    for col <- @last_col..1, do: print_space(row, col)
+    IO.write "\n"
+  end
+
+  def print_space(row, col) do
+    ConnectFour.Space.agent_name(row, col)
+    |> Process.whereis
+    |> Agent.get(fn state -> state end)
+    |> convert_for_display
+    |> IO.write
+  end
+
+  def convert_for_display(state) do
+    case state do
+      :empty -> "."
+      :red   -> "R"
+      :black -> "B"
+      _      -> "?"
+    end
   end
 end
--- lib/connect_four/space.ex
+++ lib/connect_four/space.ex
@@ -1,6 +1,9 @@
 defmodule ConnectFour.Space do
-  def start_link({row, column}) do
-    name = String.to_atom("R#{row}C#{column}")
-    Agent.start_link(fn -> Empty end, [name: name])
+  def start_link({row, col}) do
+    Agent.start_link(fn -> :empty end, [name: agent_name(row, col)])
+  end
+
+  def agent_name(row, col) do
+    String.to_atom("R#{row}C#{col}")
   end
 end
